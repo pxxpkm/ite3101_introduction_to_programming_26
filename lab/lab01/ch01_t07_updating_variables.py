e5c1b919265c74bd47b7d0d5c6038fa90dd1a3be
LeoPoon <pccpkm@gmail.com>
2026-09-08 03:06:19 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -1,5 +1,4 @@
-# --- 1. Updating annual_rainfall (Plus-Equals +=) ---
-january_to_june_rainfall = 5.16 + 7.20 + 5.06 + 4.06
+january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
 annual_rainfall = january_to_june_rainfall
 
 july_rainfall = 1.05
@@ -12,17 +11,3 @@ september_rainfall = 5.16
 october_rainfall = 7.20
 november_rainfall = 5.06
 december_rainfall = 4.06
-
-# Add remaining months using +=
-annual_rainfall += september_rainfall
-annual_rainfall += october_rainfall
-annual_rainfall += november_rainfall
-annual_rainfall += december_rainfall
-
-
-# --- 2. Instructions 1 & 2 ---
-# 1. Multiply two numbers together
-product = 12 * 4
-
-# 2. Find remainder when 1398 is divided by 11
-remainder = 1398 % 11  # Evaluates to 1
