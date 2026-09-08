f0b52e309a45a4a3aca21d0941763460687584ff
LeoPoon <pccpkm@gmail.com>
2026-09-08 03:26:08 +0000

    [skip actions] lab/lab01/ch01_t12_boolean.py

--- lab/lab01/ch01_t12_boolean.py
+++ lab/lab01/ch01_t12_boolean.py
@@ -1,3 +1,8 @@
 # Hi! I'm Maria and I live in script.py.
 # I'm an expert Python coder.
 # I'm 21 years old and I plan to program cool stuff forever.
+ # Create age_is_12 variable
+age_is_12 = True
+
+# Create name_is_maria variable
+name_is_maria = True
