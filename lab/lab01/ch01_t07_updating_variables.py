10e38a02e4dc4491b99c397ab45298a4102b6204
LeoPoon <pccpkm@gmail.com>
2026-09-08 02:59:55 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -1,4 +1,4 @@
-january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
+january_to_june_rainfall = 2.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
 annual_rainfall = january_to_june_rainfall
 
 july_rainfall = 1.05
