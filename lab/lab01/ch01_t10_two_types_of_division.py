521b6a74bfe12951ab7c7fef7d711496c1e17853
LeoPoon <pccpkm@gmail.com>
2026-09-08 03:24:03 +0000

    [skip actions] lab/lab01/ch01_t11_multi_line_strings.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -1,10 +1,6 @@
 cucumbers = 100
 num_people = 6
-
-# 2. Integer division (floor division using //)
 whole_cucumbers_per_person = cucumbers // num_people
 print(whole_cucumbers_per_person)
-
-# 3. Regular float division (using /)
 float_cucumbers_per_person = cucumbers / num_people
 print(float_cucumbers_per_person)
