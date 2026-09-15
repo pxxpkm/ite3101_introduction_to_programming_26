7ebe8c5044c864205990d425183e59c2f04c50b0
LeoPoon <pccpkm@gmail.com>
2026-09-15 02:06:01 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -2,6 +2,6 @@
 
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
-# print(caesar)
-# print(praline)
-# print(viking)
+print(caesar)
+print(praline)
+print(viking)
